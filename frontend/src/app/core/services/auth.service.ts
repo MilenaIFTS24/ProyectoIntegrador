0f4724cc078f14e45ac6e39ae8e16ca75db9bab8
Milena Lingua <39387464@ifts24.edu.ar>
2026-03-19 12:06:59 -0300

    Servicio auth: login y logout.

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  public isLoggedIn = signal<boolean>(!!localStorage.getItem('userToken'));
+
   constructor() { }
+
+  login(token: string) {
+    localStorage.setItem('userToken', token);
+    this.isLoggedIn.set(true);
+  }
+
+  logout() {
+    localStorage.removeItem('userToken');
+    this.isLoggedIn.set(false);
+  }
+
 }
