e1a6cc2b1e101e2b08e57d0b97147110f4ca0198
Milena Lingua <39387464@ifts24.edu.ar>
2026-03-23 13:31:07 -0300

    Corrección lógica authservice. Formulario login y ts bases.

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,13 +1,38 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { ApiService } from './api.service'; // Ajusta la ruta
+import { User } from '../models/user.model';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  private api = inject(ApiService);
+  
+  // Tu Signal para saber si hay alguien conectado
   public isLoggedIn = signal<boolean>(!!localStorage.getItem('userToken'));
 
-  constructor() { }
+
+  loginAction(credentials: any): Observable<any> {
+    return this.api.post<any>('auth/login', credentials).pipe(
+      tap(res => {
+        if (res && res.token) {
+          this.login(res.token); // actualizo el signal
+        }
+      })
+    );
+  }
+
+  register(userData: User): Observable<any> {
+    return this.api.post<any>('auth/register', userData).pipe(
+      tap(res => {
+        // Para registro con logueo automatico.
+        if (res && res.token) {
+          this.login(res.token);
+        }
+      })
+    );
+  }
 
   login(token: string) {
     localStorage.setItem('userToken', token);
@@ -18,5 +43,4 @@ export class AuthService {
     localStorage.removeItem('userToken');
     this.isLoggedIn.set(false);
   }
-
 }
--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+// login.component.ts
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+//import { NotificationService } from '../../../core/services/notification.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  //private notify = inject(NotificationService);
+  private router = inject(Router);
 
+  public loading = false;
+
+  public loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    const credentials = this.loginForm.getRawValue();
+
+    this.authService.loginAction(credentials).subscribe({
+      next: () => {
+        // El 'res.token' lo manejamos en el servicio (vimos el .pipe(tap) antes)
+        //this.notify.toast('¡Bienvenido de nuevo!', 'success');
+        alert('Bienvenido de nuevo!');
+        this.router.navigate(['/adminDashboard']); // Redirigimos al panel
+      },
+      error: (err: any) => {
+        this.loading = false;
+        // Error 401: Credenciales incorrectas
+        const msg = err.status === 401 ? 'Correo o contraseña incorrectos' : 'Error de conexión';
+        //this.notify.toast(msg, 'error');
+        alert(msg);
+      }
+    });
+  }
 }
