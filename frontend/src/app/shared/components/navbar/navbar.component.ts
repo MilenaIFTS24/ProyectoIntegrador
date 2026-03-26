0cc289d1b9a03e68e63964264614b19ed827ec18
Milena Lingua <39387464@ifts24.edu.ar>
2026-03-26 13:52:12 -0300

    Logica navbar y html con css mas completos acordes a la estetica del proyecto.

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+//import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  // Inyectamos el AuthService para acceder a los Signals
+  // Lo ponemos público para que el HTML pueda leerlo directamente
+  //public authService = inject(AuthService);
 
+  logout() {
+  //  this.authService.logout();
+  }
 }
